db8932798694cc5731dd70f287b96f034505d244
Marcos Tuio da Rocha Pereira <148136501+Marcostrp@users.noreply.github.com>
2026-03-27 21:24:40 -0300

    Modify cached assets in service worker
    Updated cached assets to include index.html and Google Fonts.

--- sw.js
+++ sw.js
@@ -1,5 +1,11 @@
 const CACHE_NAME = 'setlist-cache-v1';
-const assets = ['./setlist-app.html', './manifest.json'];
+// Corrigido para index.html e adicionado fontes
+const assets = [
+  './',
+  './index.html', 
+  './manifest.json',
+  'https://fonts.googleapis.com/css2?family=Bebas+Neue&family=DM+Sans:wght@300;400;500;600&family=DM+Mono:wght@400;500&display=swap'
+];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
